967b87a7fd1527ece316f95b9534de583656501a
pako8101 <pako810129@gmail.com>
2024-05-23 20:19:29 +0300

    Initial commit

---
+++ src/main/java/TouristPathfinder2024/TouristPathfinder2024Application.java
@@ -0,0 +1,13 @@
+package TouristPathfinder2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristPathfinder2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristPathfinder2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/TouristPathfinder2024/TouristPathfinder2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package TouristPathfinder2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristPathfinder2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
